929a0c2bf38592461546c68857a429ffe748c31b
Roberto Barbosa <robertojf95@gmail.com>
2024-07-25 19:02:22 -0300

    Add commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Github pages 
-  // Comment to run dev
-  output: "export",
-  basePath: "/nextjs-github-pages",
-  images: {
-    unoptimized: true,
-  },
-  // end
   reactStrictMode: true,
 
 };
--- src/pages/components/logo.tsx
+++ src/pages/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const Logo = () => {
   return (
     <Box padding="14">
-      <Image width="200" alt="Logo da Kunlatek" src="/nextjs-github-pages/logo.png" />
+      <Image width="200" alt="Logo da Kunlatek" src="./logo.png" />
     </Box>
   );
 };
